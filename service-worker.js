c02e081150c2504580c285cbb01b8efcce07d04c
ahmadyusefjumaa <ahmad.yusef.jumaa@gmail.com>
2026-09-08 23:09:31 +0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -3,8 +3,8 @@ const APP_SHELL = [
   "./",
   "./index.html",
   "./manifest.json",
-  "./icons/icon-192.svg",
-  "./icons/icon-512.svg"
+  "./icon-192.svg",
+  "./icon-512.svg"
 ];
 
 self.addEventListener("install", event => {
